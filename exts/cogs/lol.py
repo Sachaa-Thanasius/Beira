6278c6d5632d64cb974a0d1a1a186eaecab0b319
ralph.cheng <ralph.cheng@paya.com>
2023-01-18 17:41:12 -0500

    added update button, idk if it works

--- exts/cogs/lol.py
+++ exts/cogs/lol.py
@@ -15,6 +15,9 @@ from discord.ext import commands
 
 from utils.embeds import StatsEmbed
 
+from selenium import webdriver
+import time
+
 if TYPE_CHECKING:
     from bot import Beira
 
@@ -34,7 +37,7 @@ class LoLCog(commands.Cog):
             "BobbaExpress",
             "SleepyLunatic",
             "Law of Shurima",
-            "HowaryByyi"
+            "ogyrfr"
         ]
         self.req_site = "https://www.op.gg/summoners/na/"
         self.req_headers = {
@@ -66,7 +69,8 @@ class LoLCog(commands.Cog):
             embed = StatsEmbed(color=0x193d2c, title=title,
                                description="This player either doesn't exist or isn't ranked!")
         else:
-            embed = StatsEmbed(stat_headers=stat_headers, record=stats, color=0x193d2c, title=title)
+            embed = StatsEmbed(stat_headers=stat_headers,
+                               record=stats, color=0x193d2c, title=title)
 
         await ctx.send(embed=embed)
 
@@ -83,16 +87,19 @@ class LoLCog(commands.Cog):
         """
 
         # Get a default list of summoners if nothing was input.
-        summoner_name_list = summoner_name_list.split() if summoner_name_list else self.default_summoners_list
+        summoner_name_list = summoner_name_list.split(
+        ) if summoner_name_list else self.default_summoners_list
 
         # Get the information for every user.
         tasks = []
         for name in summoner_name_list:
-            tasks.append(asyncio.get_event_loop().create_task(self.check_winrate(name)))
+            tasks.append(asyncio.get_event_loop().create_task(
+                self.check_winrate(name)))
             await asyncio.sleep(0.25)
         results = await asyncio.gather(*tasks)
 
-        leaderboard = list(filter(lambda x: x != ("None", "None", "None"), results))
+        leaderboard = list(
+            filter(lambda x: x != ("None", "None", "None"), results))
         leaderboard.sort(key=lambda x: x[2])
 
         # Construct the embed for the leaderboard.
@@ -106,7 +113,8 @@ class LoLCog(commands.Cog):
         if leaderboard:
             for rank, row in enumerate(leaderboard):
                 entity_stats = f"({row[1]} \|| {row[2]})"
-                embed.add_field(name=f":medal: **{rank + 1} | {row[0]}**", value=entity_stats, inline=False)
+                embed.add_field(
+                    name=f":medal: **{rank + 1} | {row[0]}**", value=entity_stats, inline=False)
 
         await ctx.send(embed=embed)
 
@@ -127,6 +135,8 @@ class LoLCog(commands.Cog):
         adjusted_name = urllib.parse.quote(summoner_name)
         url = self.req_site + adjusted_name
 
+        self.selenium_update(url)
+
         try:
             async with self.bot.web_session.get(url, headers=self.req_headers) as response:
                 text = await response.text()
@@ -141,6 +151,21 @@ class LoLCog(commands.Cog):
             summoner_name, winrate, rank = "None", "None", "None"
 
         return summoner_name, winrate, rank
+
+    def selenium_update(self, url: str) -> None:
+        # Create the webdriver object. Here the
+        # chromedriver is present in the driver
+        # folder of the root directory.
+        driver = webdriver.Chrome(r"./driver/chromedriver")
+
+        # get https://www.geeksforgeeks.org/
+        driver.get(url)
+
+        driver.maximize_window()
+        time.sleep(10)
+
+        button = driver.find_element_by_link_text("Update")
+        button.click()
 
 
 async def setup(bot: Beira):
