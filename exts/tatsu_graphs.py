710154d5fa3606b8cf38e7290c5340ee57a7adab
Thanos <111999343+Sachaa-Thanasius@users.noreply.github.com>
2023-06-20 15:17:16 -0400

    Create tatsu_graphs.py

---
+++ exts/tatsu_graphs.py
@@ -0,0 +1,93 @@
+"""
+tatsu_graphs.py: A cog for doing stuff with Tatsu's API.
+
+References
+----------
+https://dev.tatsu.gg/
+https://github.com/Sachaa-Thanasius/Tatsu
+"""
+
+import logging
+from io import BytesIO
+from typing import ClassVar
+
+import discord
+import matplotlib.pyplot as plt
+import numpy as np
+import tatsu
+from discord.ext import commands
+
+import core
+
+
+LOGGER = logging.getLogger(__name__)
+
+
+class TatsuCog(commands.Cog, name="Tatsu"):
+    """A cog for doing stuff with Tatsu's API."""
+
+    API_KEY: ClassVar[str]
+
+    def __init__(self, bot: core.Beira) -> None:
+        self.bot = bot
+        self.tatsu_client = tatsu.Client(self.API_KEY)
+
+    async def cog_unload(self) -> None:
+        await self.tatsu_client.close()
+
+    async def cog_command_error(self, ctx: core.Context, error: Exception) -> None:
+        LOGGER.error("", exc_info=error)
+
+    @staticmethod
+    def process_data(data: np.ndarray) -> BytesIO:
+        # Set colors for the points.
+        raw_colors = np.array(["orange", "green", "blue"])
+        length, leftover = np.divmod(len(data), len(raw_colors))
+        colors = np.concatenate((np.tile(raw_colors, length), np.array(raw_colors[:leftover])))
+
+        # Plot the data with matplotlib.
+        fig, ax = plt.subplots()
+        ax.scatter(data["rank"], data["score_rate"], c=colors)
+        ax.set_xlabel("Current Rank")
+        ax.set_ylabel("Average Score Per Day")
+        ax.set_title("The Average Points per Day Earned\nby the Top 25 Active Server Members")
+
+        buff = BytesIO()
+        plt.savefig(buff, format="png")
+        buff.seek(0)
+        return buff
+
+    @commands.command()
+    @commands.is_owner()
+    async def graph(self, ctx: commands.Context, guild_id: int | None = None) -> None:
+        async with ctx.typing():
+            now = discord.utils.utcnow()
+
+            # Check that there's a guild ID to work with.
+            query_id = guild_id or ctx.guild.id
+            if not query_id:
+                await ctx.send("You must do this in a guild or provide a valid guild ID.")
+                return
+
+            query_guild = self.bot.get_guild(query_id)
+
+            results = await self.tatsu_client.get_guild_rankings(query_id, "all", end=25)
+            data = np.array(
+                [
+                    ((ranking.score / (now - valid_member.joined_at).days), ranking.user_id, ranking.rank)
+                    for ranking in results.rankings
+                    if (valid_member := query_guild.get_member(int(ranking.user_id)))
+                ],
+                dtype=[("score_rate", "f4"), ("user_id", "U20"), ("rank", "i4")]
+            )
+
+            graph_bytes = await self.bot.loop.run_in_executor(None, self.process_data, data)
+            graph_file = discord.File(graph_bytes, "graph.png")
+            await ctx.send(file=graph_file)
+
+
+async def setup(bot: core.Beira) -> None:
+    """Connects cog to bot."""
+
+    TatsuCog.API_KEY = bot.config["tatsu"]["key"]
+    await bot.add_cog(TatsuCog(bot))
